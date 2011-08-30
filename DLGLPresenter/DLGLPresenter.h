84dc0d73573be5db83caf2894b723906f8ca1d8e
Christopher Stawarz <cstawarz@mit.edu>
2011-08-30 10:56:44 -0400

    Initial Commit

---
+++ DLGLPresenter/DLGLPresenter.h
@@ -0,0 +1,13 @@
+//
+//  DLGLPresenter.h
+//  DLGLPresenter
+//
+//  Created by Christopher Stawarz on 8/30/11.
+//  Copyright 2011 MIT. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DLGLPresenter : NSObject
+
+@end
---
+++ DLGLPresenter/DLGLPresenter.m
@@ -0,0 +1,23 @@
+//
+//  DLGLPresenter.m
+//  DLGLPresenter
+//
+//  Created by Christopher Stawarz on 8/30/11.
+//  Copyright 2011 MIT. All rights reserved.
+//
+
+#import "DLGLPresenter.h"
+
+@implementation DLGLPresenter
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
