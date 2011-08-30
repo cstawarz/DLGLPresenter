3bf6b783ea603ea3adee00a40ba01be405948dd5
Christopher Stawarz <cstawarz@mit.edu>
2011-08-30 11:41:34 -0400

    Added skeleton DLGLPresenterView class

---
+++ DLGLPresenter/DLGLPresenterView.h
@@ -0,0 +1,17 @@
+//
+//  DLGLPresenterView.h
+//  DLGLPresenter
+//
+//  Created by Christopher Stawarz on 8/30/11.
+//  Copyright 2011 MIT. All rights reserved.
+//
+
+#import <AppKit/AppKit.h>
+#import <CoreVideo/CVDisplayLink.h>
+
+
+@interface DLGLPresenterView : NSOpenGLView {
+    CVDisplayLinkRef displayLink;
+}
+
+@end
---
+++ DLGLPresenter/DLGLPresenterView.m
@@ -0,0 +1,26 @@
+//
+//  DLGLPresenterView.m
+//  DLGLPresenter
+//
+//  Created by Christopher Stawarz on 8/30/11.
+//  Copyright 2011 MIT. All rights reserved.
+//
+
+#import "DLGLPresenterView.h"
+
+
+@implementation DLGLPresenterView
+
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+
+@end
